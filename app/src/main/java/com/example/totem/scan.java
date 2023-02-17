6eb29402a3b7c8342cf48bf7f49d0e29fcbc3bdf
rmo02 <ramoon.maia23@gmail.com>
2023-02-16 22:00:45 -0300

    arruamndo telas

--- app/src/main/java/com/example/totem/scan.java
+++ app/src/main/java/com/example/totem/scan.java
@@ -10,5 +10,7 @@ public class scan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        getSupportActionBar().hide();
     }
 }
