e1009746daa21e6341d53a03731dcf388e961c85
rmo02 <ramoon.maia23@gmail.com>
2023-02-20 11:22:07 -0300

    inpute de cpf

---
+++ app/src/main/java/com/example/totem/InputCPF.java
@@ -0,0 +1,14 @@
+package com.example.totem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputCPF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_cpf);
+    }
+}
