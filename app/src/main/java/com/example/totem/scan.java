2d71b918d3c21d51533e33e5a56622af733f42e5
rmo02 <ramoon.maia23@gmail.com>
2023-03-01 18:20:34 -0300

    adicionado sdk sunmi

--- app/src/main/java/com/example/totem/scan.java
+++ app/src/main/java/com/example/totem/scan.java
@@ -1,10 +1,16 @@
 package com.example.totem;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.sunmi.scan.Config;
+import com.sunmi.scan.Image;
+import com.sunmi.scan.ImageScanner;
+import com.sunmi.scan.Symbol;
+import com.sunmi.scan.SymbolSet;
 
 public class scan extends AppCompatActivity {
+    private ImageScanner scanner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,6 @@ public class scan extends AppCompatActivity {
         setContentView(R.layout.activity_scan);
 
         getSupportActionBar().hide();
+
     }
 }
